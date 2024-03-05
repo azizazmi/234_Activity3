37d1136c2d4474bf1c15a75d0975f7073fafe858
azizazmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-03-05 18:14:03 +0700

    first commit

--- 234_TugasNilai.cpp
+++ 234_TugasNilai.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+using namespace std;
+
+int main ()
+{
+    float matematika, fisika, rerata;
+    string status;
+    cout << "Masukkan nilai matematika" << endl;
+    cin >> matematika;
+    cout << "Masukkan nilai fisika" << endl;
+    cin >> fisika;
+    rerata = (matematika + fisika) / 2;
+    if (matematika >= 70)
+    {
+        status = "Lulus";
+    }
+    else if (rerata > 60)
+    {
+        status = "Lulus";
+    }
+    else
+    {
+        status = "Gagal";
+    }
+    cout << "Rerata = " << rerata << endl;
+    cout << "Status kelulusan = " << status << endl;
+}
